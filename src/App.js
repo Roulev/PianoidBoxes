ddc138c41d73bf85edbfb41748c998f523b008a9
Maksim Rulev <roulevm@gmail.com>
2025-06-02 13:51:50 +0300

    application template

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="topBar">
+        <h1>Header</h1>
+      </div>
+      <div className="container">
+        <div className="leftField">
+          <h2>Loading Parameters</h2>
+        </div>
+        <div className="rightField">
+          <h2>Change Parametrs</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
